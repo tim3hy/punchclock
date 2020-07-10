e1879a5d12dce6155954d03f2dc355fe2fc293a6
tim3hy <tim.christophe.graf@gmail.com>
2020-07-10 09:23:14 +0200

    Entity AbwesenheitKrankheit.java erstellt

---
+++ src/main/java/ch/zli/m223/punchclock/domain/AbwesenheitKrankheit.java
@@ -0,0 +1,49 @@
+package ch.zli.m223.punchclock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class AbwesenheitKrankheit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date startDate;
+    private Date endDate;
+    private String beschreibung;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+}
